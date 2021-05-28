c315368fd3f09dac82fc751fe998d06688e3d741
Albert Greinoecker <albert.greinoecker@gmail.com>
2021-05-28 18:13:28 +0200

    add a listener for shutdown

--- src/main/java/org/htl/springweb/demo/demo/DemoApplication.java
+++ src/main/java/org/htl/springweb/demo/demo/DemoApplication.java
@@ -2,12 +2,24 @@ package org.htl.springweb.demo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication application = new SpringApplication(DemoApplication.class);
+        application.addListeners((ApplicationListener<ContextClosedEvent>) event -> {
+            System.out.println("Shutdown process initiated...");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            	System.out.println("Exception is thrown during the ContextClosedEvent" + e);
+            }
+            System.out.println("Graceful Shutdown is processed successfully");
+        });
+        application.run(args);
 	}
 
 }
