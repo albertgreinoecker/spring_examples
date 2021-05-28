aa2d3403ff722fa42c1e320bd642996caa785ef4
Albert Greinoecker <albert.greinoecker@gmail.com>
2021-05-28 07:41:10 +0200

    move filling of the booklist to constructor

--- src/main/java/org/htl/springweb/demo/demo/BookController.java
+++ src/main/java/org/htl/springweb/demo/demo/BookController.java
@@ -10,6 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BookController {
 
+	List<Book> books = new ArrayList<Book>();
+	
+	public BookController() {
+		books.add(new Book("ISB1234", "O'Brian", "Irischer Lebenslauf"));
+		books.add(new Book("ISB1554", "Christian Ullenboom ", "Java ist auch eine Insel"));
+		books.add(new Book("ISB1555", "Franz Kafka ", "Der Prozess"));
+	}
 	@RequestMapping("/book")
 	public String getBook(Model model) {
 		model.addAttribute("book", new Book("ISB1234", "O'Brian", "Irischer Lebenslauf"));
@@ -18,9 +25,6 @@ public class BookController {
 
 	@RequestMapping("/books")
 	public String getBooks(Model model) {
-		List<Book> books = new ArrayList<Book>();
-		books.add(new Book("ISB1234", "O'Brian", "Irischer Lebenslauf"));
-		books.add(new Book("ISB1554", "Christian Ullenboom ", "Java ist auch eine Insel"));
 		model.addAttribute("books", books);
 		return "books/list";
 	}
