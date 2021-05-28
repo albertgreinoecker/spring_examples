d814d4d012fdca0cd7c2004a6a18d5d132b460e7
Albert Greinoecker <albert.greinoecker@gmail.com>
2021-05-28 08:05:15 +0200

    add homecontroller to show usage of maps and @GetMapping - calls

--- src/main/java/org/htl/springweb/demo/demo/BookController.java
+++ src/main/java/org/htl/springweb/demo/demo/BookController.java
@@ -11,12 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BookController {
 
 	List<Book> books = new ArrayList<Book>();
-	
+
 	public BookController() {
 		books.add(new Book("ISB1234", "O'Brian", "Irischer Lebenslauf"));
 		books.add(new Book("ISB1554", "Christian Ullenboom ", "Java ist auch eine Insel"));
 		books.add(new Book("ISB1555", "Franz Kafka ", "Der Prozess"));
 	}
+
 	@RequestMapping("/book")
 	public String getBook(Model model) {
 		model.addAttribute("book", new Book("ISB1234", "O'Brian", "Irischer Lebenslauf"));
---
+++ src/main/java/org/htl/springweb/demo/demo/HomeController.java
@@ -0,0 +1,24 @@
+package org.htl.springweb.demo.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	Map<String, String> links = new HashMap<String, String>();
+
+	public HomeController() {
+		links.put("Buch", "book");
+		links.put("Bücher", "books");
+	}
+
+	@GetMapping("/home")
+	public String home(Map<String, Object> model) {
+		model.put("links", links);
+		return "home";
+	}
+
+}
